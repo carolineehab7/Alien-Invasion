823cafc2775066a22b11354e0d968ed19e009b2e
carolineehab7 <carolineehab24@gmail.com>
2024-04-13 13:36:03 +0200

    stack

--- ALIEN/StackADT.h
+++ ALIEN/StackADT.h
@@ -2,39 +2,39 @@
 //  created by Frank M. Carrano and Timothy M. Henry.
 //  Copyright (c) 2017 Pearson Education, Hoboken, New Jersey.
 
-/** @file QueueADT.h */
-#ifndef QUEUE_ADT_
-#define QUEUE_ADT_
+/** @file StackADT.h */
 
-template<class T>
-class QueueADT
+#ifndef STACK_ADT_
+#define STACK_ADT_
+
+template<typename T>
+class StackADT
 {
 public:
-    /** Sees whether this queue is empty.
-     @return  True if the queue is empty, or false if not. */
+    /** checks whether this stack is empty.
+     @return  True if the stack is empty, or false if not. */
     virtual bool isEmpty() const = 0;
 
-    /** Adds a new entry to the back of this queue.
-     @post  If the operation was successful, newEntry is at the
-        back of the queue.
+    /** Adds a new entry to the top of this stack.
+     @post  If the operation was successful, newEntry is at the top of the stack.
      @param newEntry  The object to be added as a new entry.
      @return  True if the addition is successful or false if not. */
-    virtual bool enqueue(const T& newEntry) = 0;
+    virtual bool push(const T& newEntry) = 0;
 
-    /** Copies the front item of the queue to the passed parameter and removes it from queue
-     @post  If the operation was successful, the front of the queue
-        would be copied to FrontEntry parameter and removed from the queue.
+    /** Copies the top item of the stack to the passed parameter and removes it from stack
+     @post  If the operation was successful, the top item of the stack
+        has been copied to TopEntry parameter.
      @return  True if the removal is successful or false if not. */
-    virtual bool dequeue(T& FrontEntry) = 0;
+    virtual bool pop(T& TopEntry) = 0;
 
-    /** Copies the front item of the queue to the passed parameter without removing it from queue
-     @pre  The queue is not empty.
-     @post  The front of the queue has been copied to FrontEntry param , and the
-        queue is unchanged.
-     @return  The front of the queue. */
-    virtual bool peek(T& FrontEntry) const = 0;
+    /** Copies the top item of the stack to the passed parameter without removing it from stack
+     @pre  The stack is not empty.
+     @post  If the operation was successful, the top item of the stack
+        has been copied to TopEntry parameter.
+     @return  True if stack is not empty or false if not. */
+    virtual bool peek(T& TopEntry) const = 0;
 
-    /** Destroys this queue and frees its memory. */
-    virtual ~QueueADT() { }
-}; // end QueueADT
+    /** Destroys this stack and frees its assigned memory. */
+    virtual ~StackADT() { }
+}; // end StackADT
 #endif
