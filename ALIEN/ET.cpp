a8dc60770b1994fcd796edd6dd9ede038f4bfff4
IT <IT@DESKTOP-A03D7MV>
2024-04-14 16:24:42 -0700

    mogbar

--- ALIEN/ET.cpp
+++ ALIEN/ET.cpp
@@ -1,5 +1,6 @@
 #include "ET.h"
 #include <iostream>
+#include "Units.h"
 using namespace std;
 
 ET::ET(int id, string type, int jt, int health, int power, int AC) :Units(id, type, jt, health, power, AC) {
